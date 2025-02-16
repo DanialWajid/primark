4c8dc1ca6606de4db9506c388bcecdb9587f1ee8
DanialWajid <danialwajid112@gmail.com>
2025-02-16 22:48:57 +0500

    go

--- routes/route.user.js
+++ routes/route.user.js
@@ -181,7 +181,7 @@ router.post("/admin/user-forgot-password", async (req, res) => {
 
     await sendResetPasswordEmail(
       user.email,
-      `http://localhost:5001/user/reset-password/${resetToken}`
+      `https://primark-production.up.railway.app/reset-password/${resetToken}`
     );
 
     res.status(200).redirect("/admin/reset-sent");
